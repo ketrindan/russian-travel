26b7c4f11bc3eb71a43a37b3ed4474aaf6d3c614
Ekaterina Danilenko <keitilins@yandex.ru>
2023-09-19 17:23:44 +0300

    language change

---
+++ index.js
@@ -0,0 +1,79 @@
+let languageContent = {
+	"ru": {
+    "title": "Путешествия по России",
+    "subtitle": "Настоящая страна не в выпусках новостей, а здесь.",
+    "caption": "ваша полка — верхняя",
+    "intro": "Чего мы там не видели?",
+    "intro-text": "По опросам ВЦИОМ, 95% россиян мечтают куда-нибудь поехать, но только 36% планируют провести отпуск в родной стране. Мол, чего мы тут, дома, не видели? На самом деле, Россия — это целая вселенная с ласковым морем юга, густыми лесами Саян и суровыми льдами плато Путорана. А ещё увидеть все эти красоты можно без миллионов на счету, загранпаспорта и многочасовых перелетов. Как, например, Вера Башмакова — смелая молодая мама, которая взяла в охапку троих детей, усадила их в свою «Ладу» и проехала 20 тысяч километров по родной стране. Мы выбрали и описали некоторые интересные места, достойные вашего отпуска.",
+    "intro-item1": "Часовых поясов",
+    "intro-item2": "Объектов природного наследия ЮНЕСКО",
+    "intro-item3": "Объектов культурного наследия ЮНЕСКО",
+    "intro-item4": "Природных заповедников",
+    "intro-item5": "Аэропортов",
+    "place1": "Куршская коса",
+    "description1-place1": "Здесь, посреди лесов и песчаных дюн, вы сможете увидеть два водных горизонта — спокойного Куршского залива с одной стороны и подёрнутого рябью волн Балтийского моря с другой. Уникальная природная зона на краю российского анклава.",
+    "description2-place1": "а этом Калининградская область не заканчивается. Для путешественника и исследователя там же по соседству — самая западная точка России, Балтийская коса, — и немецкое наследие россыпи небольших приморских городов. Атмосфера здешних мест исключает суету, окуная в спокойствие природы и запах стального, прохладного моря.",
+    "place2": "Кольский",
+    "description1-place2": "Почти весь полуостров находится за Полярным кругом. Саамская тундра, от которой на юг — тайга, а на север — Ледовитый океан, прикидывающийся Баренцевым морем.",
+    "description2-place2": "Возможно, вы смотрели Звягинцева и даже слышали историю арктического фестиваля в Териберке. Возможно, слово «Хибины» не осталось под снегом школьных воспоминаний об уроках географии. Возможно, вы не интересовались пронизывающей земную кору сверхглубокой скважиной, а от апатитов вас давно накрывает апатия. Но ваша мечта увидеть северное сияние начинает сбываться с билетом в Мурманск.",
+    "place3": "Алтай",
+    "description1-place3": "Алтай — одно из красивейших мест в России. В первую очередь из-за гор: если ехать вдоль хребта, вы увидите склоны, усыпанные соснами, горные реки и озёра. А если в откроете в автомобиле окна, сможете познакомиться с невидимым чудом здешних мест — горным воздухом.",
+    "description2-place3": "Климат на Алтае умеренный, поэтому ехать сюда лучше всего летом. Так вы увидите всё разнообразие местной флоры и фауны. По лесам Алтая бродят лоси, над хребтами летают орлы, а на равнинах пасутся косули. И знаменитые манулы — тоже обитатели Алтайского края.",
+    "place4": "Зимний Байкал",
+    "description1-place4": "Всем известен Байкал как крупнейшее озеро в мире. Многие также знают, что это самый большой источник пресной воды и одно из красивейших мест в России.",
+    "description2-place4": "Конечно, это всё так. Но Байкал ещё идеальное место для соревнований по скийорингу. Это такой вид спорта, когда лыжник привязывает себя к мотоциклу, и тандем старается развить как можно бóльшую скорость на льду. В марте 2019 года на фестивале «Байкальская миля» был поставлен мировой рекорд — 197.011 км/ч.",
+    "place5": "Карелия",
+    "description-place5": "Сибирь заканчивается не на Урале, а в Карелии: образующая тайгу сибирская лиственница не растёт западнее Водлозера. Зато здесь она вымахивает на 30 метров — леса карельских национальных парков из-за непроходимых болот никогда не знали топора. Некоторым соснам уже больше чем полтысячелетия. Прикоснитесь к живому существу, видевшему солнце раньше, чем увидал его Иван Грозный. В девственном лесу на сотню километров не встретишь тропы. А на редких тропинках деревья в паре метров от земли помечены медвежьими когтями. Чтобы все знали, кто тут хозяин.",
+    "cover-title": "До Байкала «на собаках»",
+    "cover-subtitle": "По мотивам учебной темы о Транссибе — путешествие от столицы до Байкала на электричках.",
+    "footer1": "Карты",
+    "footer2": "Погода",
+    "footer3": "Расписание",
+    "footer4": "Календарь",
+    "footer5": "Путешествия",
+  },
+
+  "en": {
+    "title": "Traveling around Russia",
+    "subtitle": "The real country is not in the news, but here.",
+    "caption": "your shelf is the top one",
+    "intro": "What didn't we see there?",
+    "intro-text": "According to VTsIOM polls, 95% of Russians dream of traveling somewhere, but only 36% plan to spend their holidays in their home country. Like, what haven’t we seen here at home? In fact, Russia is a whole universe with the gentle sea of the south, the dense forests of the Sayan Mountains and the harsh ice of the Putorana plateau. And you can see all these beauties without millions in your account, a foreign passport and many hours of flights. Like, for example, Vera Bashmakova, a brave young mother who grabbed three children, put them in her Lada and drove 20 thousand kilometers around her native country. We have selected and described some interesting places worthy of your holiday.",
+    "intro-item1": "Time zones",
+    "intro-item2": "UNESCO Natural Heritage Sites",
+    "intro-item3": "UNESCO cultural heritage sites",
+    "intro-item4": "Nature reserves",
+    "intro-item5": "Airports",
+    "place1": "Curonian Spit",
+    "description1-place1": "Here, among forests and sand dunes, you can see two water horizons - the calm Curonian Lagoon on one side and the rippling Baltic Sea on the other. A unique natural area on the edge of the Russian enclave.",
+    "description2-place1": "and this is not the end of the Kaliningrad region. For the traveler and researcher, in the same neighborhood there is the westernmost point of Russia, the Baltic Spit, and the German heritage of a scattering of small coastal towns. The atmosphere of these places eliminates fuss, immersing you in the tranquility of nature and the smell of the steely, cool sea.",
+    "place2": "Kolsky",
+    "description1-place2": "Almost the entire peninsula is located above the Arctic Circle. The Sami tundra, from which to the south is the taiga, and to the north is the Arctic Ocean, pretending to be the Barents Sea.",
+    "description2-place2": "You may have watched Zvyagintsev and even heard the story of the Arctic festival in Teriberka. Perhaps the word “Khibiny” did not remain under the snow of school memories of geography lessons. Perhaps you were not interested in the ultra-deep well piercing the earth’s crust, and apatite has long been filling you with apathy. But your dream of seeing the northern lights begins to come true with a ticket to Murmansk.",
+    "place3": "Altai",
+    "description1-place3": "Altai is one of the most beautiful places in Russia. First of all, because of the mountains: if you drive along the ridge, you will see slopes strewn with pine trees, mountain rivers and lakes. And if you open the car windows, you can get acquainted with the invisible miracle of these places - the mountain air.",
+    "description2-place3": "The climate in Altai is moderate, so it is best to go here in the summer. This way you will see all the diversity of local flora and fauna. Elks roam the forests of Altai, eagles fly over the ridges, and roe deer graze on the plains. And the famous Pallas' cat is also an inhabitant of the Altai region.",
+    "place4": "Winter Baikal",
+    "description1-place4": "Everyone knows Baikal as the largest lake in the world. Many also know that this is the largest source of fresh water and one of the most beautiful places in Russia.",
+    "description2-place4": "Of course, this is all true. But Baikal is also an ideal place for skioring competitions. This is a sport where a skier straps himself to a motorcycle and the tandem tries to develop as much speed as possible on the ice. In March 2019, at the Baikal Mile festival, a world record was set - 197.011 km/h.",
+    "place5": "Karelia",
+    "description-place5": "Siberia ends not in the Urals, but in Karelia: the Siberian larch that forms the taiga does not grow west of Vodlozero. But here it reaches 30 meters—the forests of Karelian national parks have never seen an axe, due to impenetrable swamps. Some pine trees are already more than half a millennium old. Touch a living creature that saw the sun before Ivan the Terrible saw it. In a virgin forest you won’t find a trail for a hundred kilometers. And on rare paths, trees a couple of meters from the ground are marked with bear claws. So that everyone knows who is the boss here.",
+    "cover-title": "To Baikal «on dogs»",
+    "cover-subtitle": "Based on the educational topic about the Trans-Siberian Railway - a journey from the capital to Lake Baikal by train.",
+    "footer1": "Maps",
+    "footer2": "Weather",
+    "footer3": "Schedule",
+    "footer4": "Calendar",
+    "footer5": "Trips",
+  },
+}
+
+function swithcLang(lang) {
+  const langBtns = document.querySelectorAll('.header__lang-btn');
+  console.log(langBtns)
+  langBtns.forEach(i => i.classList.toggle('header__lang-btn_active'));
+
+  for (let key in languageContent[lang]) {
+    document.getElementById(key).innerHTML = languageContent[lang][key];
+  }
+}
